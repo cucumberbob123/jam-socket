ae6c767b8574582b3aeb5573a894b318e6b521d3
Cucumberbob <cucumberdoesminecraft@gmail.com>
2019-03-09 13:59:06 +0000

    loads drawing in from others, dissapears after 5

--- server.js
+++ server.js
@@ -1,73 +1,84 @@
-var express = require('express');
+let drawing = [];
+let timer;
+var express = require("express");
 
 var app = express();
 
 var server = app.listen(process.env.PORT || 3000, listen);
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 
-app.use(require('cookie-parser')());
+app.use(require("cookie-parser")());
 
 function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Sockets listening at http://' + host + ':' + port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Sockets listening at http://" + host + ":" + port);
 }
 
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-var io = require('socket.io')(server);
+var io = require("socket.io")(server);
 
-let sockets = [];
-const draw = io.sockets.on('connection',
-  // We are given a websocket object in our function
-  function (socket) {
-    console.log(sockets)
-    console.log("We have a new client: " + socket.id);
+const draw = io.sockets.on("connection",
+	// We are given a websocket object in our function
+	socket => {
+		console.log("We have a new client: " + socket.id);
 
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('mouse',
-      function (data) {
-        // Send it to all other clients
-        socket.broadcast.emit('mouse', data);
-      }
-    );
+		// When this user emits, client side: socket.emit('otherevent',some data);
+		socket.on("mouse", data => {
+			drawing.push(data);
+			if (timer) clearInterval(timer)
+			timer = setInterval(() => {
+				drawing = []
+			}, 5 * 1000 * 60)
+			// Send it to all other clients
+			socket.broadcast.emit("mouse", data);
+		});
+		socket.on("rect", data => {
+			socket.broadcast.emit("rect", data);
+		});
+		socket.on("clear", () => {
+			console.log("clearing");
+			socket.broadcast.emit("clear");
+		});
 
-    socket.on('clear', () => {
-      console.log("clearing")
-      socket.broadcast.emit('clear');
-    })
-
-    socket.on('disconnect', function () {
-      console.log("Client has disconnected");
-    });
-  }
-)
+		socket.on("disconnect", () => {
+			console.log("Client has disconnected");
+		});
+	}
+);
 
 app.post("/login", (req, res) => {
-  console.log(req.body.password)
-  if (req.body.password === "very secure password") {
-    res.cookie(
-      "authorised", true
-    )
-  }
-  return res.redirect("/")
-})
+	console.log(req.body.password);
+	if (req.body.password === "very secure password") {
+		res.cookie(
+			"authorised", true
+		);
+	}
+	return res.redirect("/");
+});
 
 app.get("/authorised", (req, res) => res.json({
-  authorised: req.cookies.authorised
-}))
+	authorised: req.cookies.authorised
+}));
 
 app.get("/clear", (req, res) => {
-  console.log("clear request")
-  if (req.cookies.authorised) {
-    console.log("clearing all canvases")
-    draw.emit("clear")
-    return res.status(200).end()
-  }
-  console.log("not authorised")
-  return res.status(200).end()
+	console.log("clear request");
+	if (req.cookies.authorised) {
+		console.log("clearing all canvases");
+		draw.emit("clear");
+		drawing = [];
+		return res.status(200).end();
+	}
+	console.log("not authorised");
+	return res.status(200).end();
+});
+
+app.get('/load', (req, res) => {
+	console.log(drawing.length)
+	return res.json(drawing)
 })
--- website/sketch.js
+++ website/sketch.js
@@ -1,88 +1,204 @@
 let socket;
 let available = true;
 
-let h = 0
-let s = 0
-let v = 100
+let h = 0;
+let s = 0;
+let v = 100;
 
 let mode = "";
 let effect;
 
+let squareCoordinates, circleCoordinates;
+
 function setup() {
-  let canvas = createCanvas(1000, 1000);
-  canvas.parent("canvas-holder");
-  background(0);
-  colorMode(HSB, 100);
-  socket = io.connect("https://jam-socket.herokuapp.com");
+	const slider = document.getElementById("slide");
+	slider.oninput = handleSlide;
+	let canvas = createCanvas(1000, 1000);
+	canvas.parent("canvas-holder");
+	background(0);
+	colorMode(HSB, 100);
+	load();
+	socket = io.connect("localhost:3000");
 
-  socket.on("clear", () => {
-    if (available) {
-      clearCanvas();
-    }
-  });
+	socket.on("clear", () => {
+		if (available) {
+			clearCanvas();
+		}
+	});
 
-  socket.on("mouse", function (data) {
-    strokeWeight(10);
-    stroke(data.h, data.s, data.v); //set color to blue
-    line(data.x, data.y, data.px, data.py);
-  });
+	socket.on("mouse", function (data) {
+		strokeWeight(10);
+		stroke(data.h, data.s, data.v);
+		line(data.x, data.y, data.px, data.py);
+	});
+
+	socket.on("rect", data => {
+		console.log(data);
+		fill(h, s, v);
+		stroke(h, s, v);
+		rect(data.x, data.y, data.px, data.py);
+	});
 }
 
-function draw() {
-  // Nothing
-}
+function draw() {}
 
 function mouseDragged() {
-  strokeWeight(10)
-  stroke(h, s, v)
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  sendmouse(mouseX, mouseY, pmouseX, pmouseY);
+	strokeWeight(10);
+	stroke(h, s, v);
+	line(mouseX, mouseY, pmouseX, pmouseY);
+	sendmouse(mouseX, mouseY, pmouseX, pmouseY);
 
 }
 
 function sendmouse(x, y, px, py) {
-  var data = {
-    x,
-    y,
-    px,
-    py,
-    h,
-    s,
-    v
-  };
-  // Send that object to the socket
-  socket.emit("mouse", data);
+	var data = {
+		x,
+		y,
+		px,
+		py,
+		h,
+		s,
+		v
+	};
+	// Send that object to the socket
+	socket.emit("mouse", data);
 }
 
 function clearCanvas() {
-  available = false;
-  fetch("/clear");
-  clear();
-  background(0);
-  setTimeout(() => (available = true), 5000);
+	available = false;
+	fetch("/clear").then(() => {
+
+		clear();
+		background(0);
+	})
+	setTimeout(() => (available = true), 5000);
 }
 
 function keyTyped() {
-  console.log(key);
-  if (key === "r") {
-    h = s = v = 100;
-    mode = "red";
-  }
-  if (key === "t") {
-    mode = "rainbow";
-    s = v = 100;
-    h = 0;
-    effect = setInterval(() => {
-      h++;
-      if (h === 100) h = 0;
-      console.log(h)
-    }, 15)
-  }
-  if (key === "n") {
-    clearInterval(effect);
-    s = 0;
-    v = 100;
-    mode = "normal"
-  }
-  console.log(mode)
+	console.log(key);
+	if (key === "r") {
+		h = s = v = 100;
+		mode = "red";
+		updateLine();
+	}
+	if (key === "t") {
+		mode = "rainbow";
+		s = v = 100;
+		h = 0;
+		clearInterval(effect)
+		effect = setInterval(() => {
+			h++;
+			if (h === 100) h = 0;
+			updateLine();
+
+		}, 15);
+	}
+	if (key === "n") {
+		clearInterval(effect);
+		s = 0;
+		v = 100;
+		mode = "normal";
+		updateLine();
+	}
+	if (key === "s") {
+		console.log(squareCoordinates);
+		if (!squareCoordinates) {
+			squareCoordinates = [mouseX, mouseY];
+		} else {
+			fill(h, s, v);
+			stroke(h, s, v);
+			squareCoordinates[2] = 20;
+			squareCoordinates[3] = 20;
+			rect(squareCoordinates[0], squareCoordinates[1], mouseX - squareCoordinates[0], mouseY - squareCoordinates[1]);
+			sendRect(squareCoordinates[0], squareCoordinates[1], mouseX - squareCoordinates[0], mouseY - squareCoordinates[1]);
+			squareCoordinates = undefined;
+		}
+		mode = "square";
+	}
+	if (key === "c") {
+		if (!circleCoordinates) {
+			circleCoordinates = [mouseX, mouseY];
+		} else {
+			fill(h, s, v);
+			stroke(h, s, v);
+			xSize = (circleCoordinates[0] - mouseX);
+			ySize = (circleCoordinates[1] - mouseY);
+
+			size = sqrt((Math.pow(xSize, 2)) + (Math.pow)) / 2;
+
+			x = circleCoordinates[0] + (xSize / 2);
+			y = circleCoordinates[1] + (ySize / 2);
+			ellipse(x, y, xSize, ySize);
+			circleCoordinates = undefined;
+		}
+		mode = "circle";
+	}
+	console.log(mode);
+}
+
+function handleSlide() {
+	s = v = 100;
+	h = this.value;
+	updateLine();
+}
+
+function updateLine() {
+	strokeWeight(10);
+	stroke(h, s, v);
+	line(1000, 0, 1000, 1000);
+}
+
+function sendRect(x, y, px, py) {
+	socket.emit("rect", {
+		x,
+		y,
+		px,
+		py,
+		h,
+		s,
+		v
+	});
+}
+
+let rectangle, circle;
+
+function mouseMoved() {
+	if (mode === "square") {
+		if (rectangle) remove(rectangle);
+		fill(h, s, v);
+		stroke(h, s, v);
+		rect(squareCoordinates[0], squareCoordinates[1], mouseX - squareCoordinates[0], mouseY - squareCoordinates[1]);
+	}
+	if (mode === "circle") {
+		if (circle) remove(circle);
+		fill(h, s, v);
+		stroke(h, s, v);
+		fill(h, s, v);
+		stroke(h, s, v);
+		let xSizen = (circleCoordinates[0] - mouseX);
+		let ySizen = (circleCoordinates[1] - mouseY);
+
+		let sizen = sqrt((Math.pow(xSizen, 2)) + (Math.pow(ySizen, 2))) / 2;
+
+		let xn = circleCoordinates[0] - (xSizen / 2);
+		let yn = circleCoordinates[1] - (ySizen / 2);
+		ellipse(xn, yn, sizen);
+	}
+}
+
+function load() {
+	const start = Date.now()
+	fetch('/load').then(data => data.json()).then(res => {
+		const loopStart = Date.now()
+		for (let i in res) {
+			const data = res[i]
+			strokeWeight(10);
+			stroke(data.h, data.s, data.v);
+			line(data.x, data.y, data.px, data.py);
+		}
+		finish = Date.now()
+		console.log(`Total Load Time: ${finish - start}`)
+		console.log(`Request Took: ${loopStart - start}`)
+		console.log(`Render Load Time: ${finish - loopStart}`)
+	})
 }
